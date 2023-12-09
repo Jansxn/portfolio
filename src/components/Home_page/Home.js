66cc1f4514ccc0bd5a6c4eb6814745d620fe7c28
Jason Shaun DMello <jason.dmello21@gmail.com>
2023-12-09 21:51:56 +0530

    Portfolio now on backend

--- src/components/Home_page/Home.js
+++ src/components/Home_page/Home.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./Home.css";
 import illus from "../../resources/pavan 772.svg";
 import linkedin from "../../resources/linkedin.png";
-import resume from "../../resources/resume.pdf";
+import oldresume from "../../resources/resume.pdf";
+
+import { storage } from "../../config/firebase";
+import {getDownloadURL, ref} from 'firebase/storage';
 
 function Home() {
+    var [resume, setResume] = useState(oldresume);
+
+    useEffect(() => {
+        getDownloadURL(ref(storage, 'JasonDMello_resume_main.pdf')).then((url) => {
+            setResume(url);
+        });
+    }, []);
+
     return (
         <div className="h" id = "home" style={{width:'100%'}}>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
