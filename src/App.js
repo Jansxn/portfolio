c6057e30d38fde16196d2faff29a3d08a88149f6
Jason Shaun DMello <jason.dmello21@gmail.com>
2024-01-15 23:26:44 +0530

    Fixed preloader bugs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import './App.css';
+import "./App.css";
 
-import Navbar from './components/Navbar_files/Navbar';
-import Home from './components/Home_page/Home';
-import About from './components/About_page/About';
-import Projects from './components/Projects_page/Projects';
-import Experience from './components/Experience_page/Experience';
-import Contact from './components/Contact_page/Contact';
+import Navbar from "./components/Navbar_files/Navbar";
+import Home from "./components/Home_page/Home";
+import About from "./components/About_page/About";
+import Projects from "./components/Projects_page/Projects";
+import Experience from "./components/Experience_page/Experience";
+import Contact from "./components/Contact_page/Contact";
+
+function hidePreloader() {
+  const loader = document.querySelector(".pre-loader");
+  if (loader) {
+    loader.classList.add("pre-loader--hidden");
+
+    loader.addEventListener("transitionend", () => {
+      loader.classList.add("pre-loader--hide");
+    });
+  }
+}
 
 function App() {
-    window.addEventListener("load", () => {
-      const loader = document.querySelector(".pre-loader");
-  
-      if (loader) {
-        loader.classList.add("pre-loader--hidden");
-  
-        loader.addEventListener("transitionend", () => {
-          loader.style.display = "none";
-        });
-      }
-    });
+  // Preloader
+  const timerID = setTimeout(() => {
+    hidePreloader();
+  }, 10000);
+
+  window.addEventListener("load", () => {
+    clearTimeout(timerID);
+    hidePreloader();
+  });
+
   return (
     <div className="App">
       <div className="pre-loader">
-        <div className='dot'></div>
-        <div className='dot'></div>
-        <div className='dot'></div>
-        <div className='dot'></div>
-        <div className='dot'></div>  
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
       </div>
       <Navbar />
       <Home />
